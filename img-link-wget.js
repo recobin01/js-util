f1c662285fb7e5417c22f9abbedbfb2f126be6af
recobin01 <recobin@yahoo.com>
2025-03-04 21:47:03 +0700

    Update img-link-wget.js

--- img-link-wget.js
+++ img-link-wget.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name        Image Link wget
 // @namespace   Violentmonkey Scripts
-// @match       https://e-hentai.org/g/3243997/8b49b95dc4/*
 // @grant       none
 // @version     1.0
 // @include     https://nhentai.net/g/*
@@ -13,6 +12,21 @@
 // ==/UserScript==
 
 
+function $(content){
+  let div = document.createElement("div")
+  if(!content) return div
+  div.innerHTML = content.trim();
+  return div.firstChild
+}
+function normalize(src){
+  if(!src) return ""
+  let idx = src.lastIndexOf(".")
+  if(idx < 0) throw new Error("normalize link not have file extension")
+  let ext = src.substring(idx)
+  if(src.endsWith(ext + ext))
+    return src.substring(0, idx)
+  return src
+}
 function nhentaiImg(){
 	let $button = document.getElementById("download")
 	let $img = document.querySelector("#thumbnail-container img");
@@ -25,31 +39,44 @@ function nhentaiImg(){
 	let pages = Array.prototype.filter.call(document.querySelectorAll("section#tags>div"), (div) => div.textContent.indexOf("Pages") > 0)[0].children[0].textContent.trim()
 
 	$button.classList.remove("btn-disabled");
-	let src = $img.src
-	src = src.replace(/t\d\./,"i2.").replace("1t", `{1..${pages}}`)
+	let src = normalize($img.src)
+	src = src.replace(/t\d\./,"i1.")
+  if(src.endsWith(".webp.webp")) src = src.sub
 
-	$button.onclick = () => { navigator.clipboard.writeText(src);}
+  let $wget = $("<a class='btn btn-secondary'>Wget Link</a>")
+  $wget.onclick = () => { navigator.clipboard.writeText(src.replace("1t", `{1..${pages}}`))}
+  $button.parentElement.appendChild($wget)
+
+  let $themall =  $("<a class='btn btn-secondary'>ThemAll Link</a>")
+  $themall.onclick = () => { navigator.clipboard.writeText(src.replace("1t", `[1:${pages}]`))}
+  $button.parentElement.appendChild($themall)
+
 }
 
 function _3hentaiImg(){
-	let $button = document.querySelector("#main-info a.btn")
-	let $img = document.querySelector("#main-cover img");
-
-	if(!$button || !$img || $img.src.indexOf("data") == 0){
-    	setTimeout(todo, 2000)
-    	return
-	}
-	let $newButton = document.createElement("a");
-	$newButton.textContent = "Copy link"
-	$newButton.className = "btn btn-primary"
-	$button.parentElement.append($newButton)
-
-	$newButton.onclick = (event) => {
-    	let src = $img.src
+  let $button = document.querySelector("#main-info a.btn")
+  let $img = document.querySelector("#main-cover img");
+	
+  if(!$button || !$img || $img.src.indexOf("data") == 0){
+    setTimeout(todo, 2000)
+    return
+  }
+	
+  let doClick = (batch) => {
+    	let src = normalize($img.src)
     	let pages = Array.prototype.filter.call(document.querySelectorAll("#main-info>div"), (div) => div.textContent.indexOf("Pages") > 0)[0].children[0].textContent.trim()
-    	let link = src.replace("cover", `{1..${pages}}`)
+    	let link = src.replace("cover", batch == "wget" ? `{1..${pages}}` : `[1:${pages}]`)
     	navigator.clipboard.writeText(link);
-	}
+  }
+
+  let $wget = $("<a class='btn btn-secondary'>Wget Link</a>")
+  $wget.onclick = () => { doClick("wget")}
+  $button.parentElement.appendChild($wget)
+
+  let $themall =  $("<a class='btn btn-secondary'>ThemAll Link</a>")
+  $themall.onclick = () => { doClick("themall")}
+  $button.parentElement.appendChild($themall)
+
 }
 
 function todo(){
