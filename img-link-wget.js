c9a31f66425fc4141c9412a75f493c0ba89e81c3
recobin01 <recobin@yahoo.com>
2025-05-08 16:20:58 +0700

    Update img-link-wget.js

--- img-link-wget.js
+++ img-link-wget.js
@@ -5,6 +5,7 @@
 // @version     1.0
 // @include     https://nhentai.net/g/*
 // @include     https://3hentai.net/d/*
+// @include     https://www.hentai.name/g/*
 // @author      -
 // @description 2/19/2025, 3:08:21 PM
 // @downloadURL https://raw.githubusercontent.com/recobin01/js-util/refs/heads/main/img-link-wget.js
@@ -25,6 +26,8 @@ function normalize(src){
   let ext = src.substring(idx)
   if(src.endsWith(ext + ext))
     return src.substring(0, idx)
+  if(src.endsWith("jpg" + ext)
+     return src.substring(0, idx - 4) + ext
   return src
 }
 function nhentaiImg(){
@@ -41,13 +44,12 @@ function nhentaiImg(){
 	$button.classList.remove("btn-disabled");
 	let src = normalize($img.src)
 	src = src.replace(/t\d\./,"i1.")
-  if(src.endsWith(".webp.webp")) src = src.sub
 
   let $wget = $("<a class='btn btn-secondary'>Wget Link</a>")
   $wget.onclick = () => { navigator.clipboard.writeText(src.replace("1t", `{1..${pages}}`))}
   $button.parentElement.appendChild($wget)
 
-  let $themall =  $("<a class='btn btn-secondary' href='" + src.replace("1t", `[1:${pages}]`) + "'>ThemAll Link</a>")
+  let $themall =  $("<a class='btn btn-secondary' href='" + src.replace("1t", `[1:${pages}]`) + "'>ThemAll [English]</a>")
   //$themall.onclick = () => { navigator.clipboard.writeText(src.replace("1t", `[1:${pages}]`))}
   $button.parentElement.appendChild($themall)
 
@@ -69,17 +71,42 @@ function _3hentaiImg(){
   $wget.onclick = () => { navigator.clipboard.writeText(src.replace("cover", `{1..${pages}}`))}
   $button.parentElement.appendChild($wget)
 
-  let $themall =  $("<a class='btn btn-secondary' href='" + src.replace("cover", `[1:${pages}]`) + "'>ThemAll Link</a>")
+  let $themall =  $("<a class='btn btn-secondary' href='" + src.replace("cover", `[1:${pages}]`) + "'>ThemAll [English]</a>")
   //$themall.onclick = () => { doClick("themall")}
   $button.parentElement.appendChild($themall)
 
 }
 
+function hentaiNameImg(){
+	let $buttons = document.querySelector("div#info div.buttons");
+	let $img = document.querySelector("div#cover a img");
+
+	if(!$buttons || !$img || !$img.src || $img.src.indexOf("data") == 0){
+    	setTimeout(todo, 2000)
+    	return
+	}
+
+	let pages = Array.prototype.filter.call(document.querySelectorAll("div#info div"), (div) => div.textContent.indexOf("pages") > 0)[0].textContent
+	pages = parseInt(pages)
+
+	let src = normalize($img.src)
+
+  let $wget = $("<a class='btn btn-secondary'>Wget Link</a>")
+  $wget.onclick = () => { navigator.clipboard.writeText(src.replace("poster", `{1..${pages}}`))}
+  $buttons.appendChild($wget)
+
+  let $themall =  $("<a class='btn btn-secondary' href='" + src.replace("poster", `[1:${pages}]`) + "'>ThemAll [English] </a>")
+  //$themall.onclick = () => { navigator.clipboard.writeText(src.replace("1t", `[1:${pages}]`))}
+  $buttons.appendChild($themall)
+
+}
 function todo(){
   if(window.location.hostname.indexOf("nhentai") >= 0){
       nhentaiImg()
   } else if(window.location.hostname.indexOf("3hentai") >= 0){
       _3hentaiImg()
+  } else if(window.location.hostname.indexOf("hentai.name") >= 0){
+      hentaiNameImg()
   }
 }
 todo();
